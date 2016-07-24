ad1b226beab015c0a7cfdcda83dde082382272ba
Yassine Rafa <yassine.rafa@outlook.com>
2016-07-25 00:09:58 +0100

    Add delete and edit Function to answers

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,5 +1,6 @@
 class AnswersController < ApplicationController
-
+  before_action :find_post, only: [:create, :edit, :update, :destroy]
+  before_action :find_answer, only: [:edit, :update, :destroy]
   def create
     @post = Post.find(params[:post_id])
     @answer = @post.answers.create(answer_params)
@@ -12,8 +13,31 @@ class AnswersController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @answer.update(answer_params)
+      redirect_to post_path(@post)
+    else
+      render 'edit'
+    end
+  end
+  def destroy
+    @answer.destroy
+    redirect_to post_path(@post)
+  end
+
   private
     def answer_params
       params.require(:answer).permit(:content)
+    end
+
+    def find_post
+      @post = Post.find(params[:post_id])
+    end
+
+    def find_answer
+      @answer = @post.answers.find(params[:id])
     end
 end
