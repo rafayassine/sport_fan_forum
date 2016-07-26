7d751ede06ae829bbf4f6a05b1d12b6fa49dcb9e
Yassine Rafa <yassine.rafa@outlook.com>
2016-07-26 20:07:46 +0100

    Add styling and authentification for answer and post controller

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,4 +1,5 @@
 class AnswersController < ApplicationController
+  before_action :authenticate_user!
   before_action :find_post, only: [:create, :edit, :update, :destroy]
   before_action :find_answer, only: [:edit, :update, :destroy]
   def create
